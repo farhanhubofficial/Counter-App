4f0c3946290e6d6f42affb3e5fda18350c4b99a0
jefferson wanyonyi <weeddyson@gmail.com>
2023-09-29 09:13:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Counter from './components/Counter'
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+
+function Counter() {
+
+  const [counter, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+        <div className="circle">
+            {counter}
+        </div>
+        {/* buttons */} 
+        <div className='btns'>
+            <button onClick={() => setCount(counter != 0 ? counter - 1 : counter = 0)}>-</button>
+            <button onClick={() => setCount(0) }>Restart</button>
+            <button onClick={() => setCount(counter + 1)}>+</button>
+        </div>
+    </div>
+  )
+}
+
+export default Counter
